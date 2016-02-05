b5150a80d5bcbe544445446b058a06baa42d9212
Thamizharasu <zarub2k@gmail.com>
2016-02-06 00:04:23 +0530

    Widget imply started

---
+++ Football_Scores/app/src/main/java/barqsoft/footballscores/changes/FootballScoreWidgetProvider.java
@@ -0,0 +1,15 @@
+package barqsoft.footballscores.changes;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+
+/**
+ * @author tham
+ */
+public class FootballScoreWidgetProvider extends AppWidgetProvider {
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        super.onUpdate(context, appWidgetManager, appWidgetIds);
+    }
+}
