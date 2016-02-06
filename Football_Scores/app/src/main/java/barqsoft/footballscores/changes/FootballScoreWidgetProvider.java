940b936272dfe1e0cb3e89497e9c9511d686a867
Thamizharasu <zarub2k@gmail.com>
2016-02-06 22:10:19 +0530

    Widget info and ui xml changes are done

--- Football_Scores/app/src/main/java/barqsoft/footballscores/changes/FootballScoreWidgetProvider.java
+++ Football_Scores/app/src/main/java/barqsoft/footballscores/changes/FootballScoreWidgetProvider.java
@@ -3,6 +3,9 @@ package barqsoft.footballscores.changes;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
+import android.widget.RemoteViews;
+
+import barqsoft.footballscores.R;
 
 /**
  * @author tham
@@ -10,6 +13,10 @@ import android.content.Context;
 public class FootballScoreWidgetProvider extends AppWidgetProvider {
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
-        super.onUpdate(context, appWidgetManager, appWidgetIds);
+        for (int appWidgetId : appWidgetIds) {
+            final RemoteViews widgetView = new RemoteViews(context.getPackageName(),
+                    R.layout.appwidget_view);
+            appWidgetManager.updateAppWidget(appWidgetId, widgetView);
+        }
     }
 }
