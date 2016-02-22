4f7cc3562234a29f9cc38b07adf9eb86eb798cc1
Thamizharasu <zarub2k@gmail.com>
2016-02-23 00:04:13 +0530

    Code started

--- alexandria/app/src/main/java/it/jaschke/alexandria/changes/BarcodeCaptureActivity.java
+++ alexandria/app/src/main/java/it/jaschke/alexandria/changes/BarcodeCaptureActivity.java
@@ -3,6 +3,8 @@ package it.jaschke.alexandria.changes;
 import android.app.Activity;
 import android.os.Bundle;
 
+import com.google.android.gms.vision.barcode.BarcodeDetector;
+
 /**
  * @author tham
  */
@@ -11,5 +13,7 @@ public class BarcodeCaptureActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        final BarcodeDetector barcodeDetector = new BarcodeDetector.Builder(this).build();
     }
 }
