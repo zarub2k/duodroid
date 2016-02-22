e921be122894866bc205abf1d30aeedc02951f6a
Thamizharasu <zarub2k@gmail.com>
2016-02-22 23:38:21 +0530

    Layout clean-up is done

---
+++ alexandria/app/src/main/java/it/jaschke/alexandria/changes/BarcodeCaptureActivity.java
@@ -0,0 +1,15 @@
+package it.jaschke.alexandria.changes;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * @author tham
+ */
+public class BarcodeCaptureActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
