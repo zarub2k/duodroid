385b3249208829ea80fe7ed1a7c2aa17ebc70ce5
Thamizharasu <zarub2k@gmail.com>
2016-02-24 00:04:02 +0530

    barcode activity is introduced

--- alexandria/app/src/main/java/it/jaschke/alexandria/changes/BarcodeCaptureActivity.java
+++ alexandria/app/src/main/java/it/jaschke/alexandria/changes/BarcodeCaptureActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 
 import com.google.android.gms.vision.barcode.BarcodeDetector;
 
+import it.jaschke.alexandria.R;
+
 /**
  * @author tham
  */
@@ -13,7 +15,15 @@ public class BarcodeCaptureActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_barcode);
+
+        startCameraPreview();
 
         final BarcodeDetector barcodeDetector = new BarcodeDetector.Builder(this).build();
+    }
+
+    private void startCameraPreview() {
+//        CameraSourcePreview preview = (CameraSourcePreview) findViewById(R.id.preview);
+        findViewById(R.id.graphicOverlay);
     }
 }
