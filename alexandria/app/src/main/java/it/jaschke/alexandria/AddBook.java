b688fcae7b371e5586e7f3b75a93c09638b4d6fe
Thamizharasu <zarub2k@gmail.com>
2016-02-23 23:37:17 +0530

    build version and dependencies changed

--- alexandria/app/src/main/java/it/jaschke/alexandria/AddBook.java
+++ alexandria/app/src/main/java/it/jaschke/alexandria/AddBook.java
@@ -20,13 +20,19 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 
+import com.google.android.gms.common.api.CommonStatusCodes;
+
+import it.jaschke.alexandria.changes.BarcodeCaptureActivity;
 import it.jaschke.alexandria.data.AlexandriaContract;
 import it.jaschke.alexandria.services.BookService;
 import it.jaschke.alexandria.services.DownloadImage;
 
 
 public class AddBook extends Fragment implements LoaderManager.LoaderCallbacks<Cursor> {
-    private static final String TAG = "INTENT_TO_SCAN_ACTIVITY";
+    private static final String LOG_TAG = AddBook.class.getSimpleName();
+
+    private static final int RC_BARCODE_CAPTURE = 9001;
+
     private EditText ean;
     private final int LOADER_ID = 1;
     private View rootView;
@@ -87,19 +93,10 @@ public class AddBook extends Fragment implements LoaderManager.LoaderCallbacks<C
         rootView.findViewById(R.id.scan_button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // This is the callback method that the system will invoke when your button is
-                // clicked. You might do this by launching another app or by including the
-                //functionality directly in this app.
-                // Hint: Use a Try/Catch block to handle the Intent dispatch gracefully, if you
-                // are using an external app.
-                //when you're done, remove the toast below.
-                Context context = getActivity();
-                CharSequence text = "This button should let you scan a book for its barcode!";
-                int duration = Toast.LENGTH_SHORT;
-
-                Toast toast = Toast.makeText(context, text, duration);
-                toast.show();
-
+                final Intent intent = new Intent(AddBook.this.getActivity(), BarcodeCaptureActivity.class);
+//                intent.putExtra(BarcodeCaptureActivity.AutoFocus, autoFocus.isChecked());
+//                intent.putExtra(BarcodeCaptureActivity.UseFlash, useFlash.isChecked());
+                startActivityForResult(intent, RC_BARCODE_CAPTURE);
             }
         });
 
@@ -200,5 +197,19 @@ public class AddBook extends Fragment implements LoaderManager.LoaderCallbacks<C
     public void onAttach(Activity activity) {
         super.onAttach(activity);
         activity.setTitle(R.string.scan);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (RC_BARCODE_CAPTURE != requestCode) {
+            super.onActivityResult(requestCode, resultCode, data);
+            return;
+        }
+
+        if (resultCode == CommonStatusCodes.SUCCESS) {
+            if (data != null) {
+//                data.getParcelableExtra(BarcodeCaptureActivity.BarCodeObject);
+            }
+        }
     }
 }
