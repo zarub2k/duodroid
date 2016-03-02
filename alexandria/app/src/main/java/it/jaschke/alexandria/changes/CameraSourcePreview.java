2f17e209b836e7f735d811c24de19f96b1b1fb91
Thamizharasu <zarub2k@gmail.com>
2016-03-02 21:09:44 +0530

    Camera source preview code changed

--- alexandria/app/src/main/java/it/jaschke/alexandria/changes/CameraSourcePreview.java
+++ alexandria/app/src/main/java/it/jaschke/alexandria/changes/CameraSourcePreview.java
@@ -1,9 +1,25 @@
 package it.jaschke.alexandria.changes;
 
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.ViewGroup;
+
 /**
  * @author tham
  *
  * Preview class for camera source
  */
-public class CameraSourcePreview {
+public class CameraSourcePreview extends ViewGroup {
+    private Context context_;
+
+    public CameraSourcePreview(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        context_ = context;
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+
+    }
 }
