821e8290bee83041ed86d420301214ad50d630c8
Thamizharasu <zarub2k@gmail.com>
2016-03-03 22:54:24 +0530

    Camera preview code is added

--- alexandria/app/src/main/java/it/jaschke/alexandria/changes/CameraSourcePreview.java
+++ alexandria/app/src/main/java/it/jaschke/alexandria/changes/CameraSourcePreview.java
@@ -4,6 +4,7 @@ import android.Manifest;
 import android.content.Context;
 import android.support.annotation.RequiresPermission;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.ViewGroup;
@@ -24,10 +25,17 @@ public class CameraSourcePreview extends ViewGroup {
     private SurfaceView surfaceView_;
     private CameraSource cameraSource_;
 
+    private boolean isStartRequested;
+    private boolean isSurfaceAvailable;
+
     public CameraSourcePreview(Context context, AttributeSet attrs) {
         super(context, attrs);
 
         context_ = context;
+
+        isStartRequested = false;
+        isSurfaceAvailable = false;
+
         surfaceView_ = new SurfaceView(context);
         surfaceView_.getHolder().addCallback(new SurfaceCallback());
         addView(surfaceView_);
@@ -45,27 +53,55 @@ public class CameraSourcePreview extends ViewGroup {
         }
 
         cameraSource_ = cameraSource;
+
+        if (cameraSource_ != null) {
+            isStartRequested = true;
+            startIfReady();
+        }
     }
 
     public void stop() {
+        if (cameraSource_ != null) {
+            cameraSource_.stop();
+        }
+    }
 
+    public void release() {
+        if (cameraSource_ != null) {
+            cameraSource_.release();
+            cameraSource_ = null;
+        }
+    }
+
+    @RequiresPermission(Manifest.permission.CAMERA)
+    private void startIfReady() throws IOException, SecurityException {
+        if (isStartRequested && isSurfaceAvailable) {
+
+        }
     }
 
     private class SurfaceCallback implements SurfaceHolder.Callback {
 
         @Override
         public void surfaceCreated(SurfaceHolder holder) {
+            isSurfaceAvailable = true;
 
+            try {
+                startIfReady();
+            } catch (IOException e) {
+                Log.e(LOG_TAG, "Could not start camera source", e);
+            } catch (SecurityException e) {
+                Log.e(LOG_TAG, "Do not have permission start camera", e);
+            }
         }
 
         @Override
         public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
-
         }
 
         @Override
         public void surfaceDestroyed(SurfaceHolder holder) {
-
+            isSurfaceAvailable = false;
         }
     }
 }
