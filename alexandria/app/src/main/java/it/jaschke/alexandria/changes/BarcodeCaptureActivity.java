18639a3681c4ad6c9bc69b921f3c4693cda91018
Thamizharasu <zarub2k@gmail.com>
2016-03-07 23:30:29 +0530

    Barcode activity code changed

--- alexandria/app/src/main/java/it/jaschke/alexandria/changes/BarcodeCaptureActivity.java
+++ alexandria/app/src/main/java/it/jaschke/alexandria/changes/BarcodeCaptureActivity.java
@@ -3,7 +3,10 @@ package it.jaschke.alexandria.changes;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.GestureDetector;
+import android.view.ScaleGestureDetector;
 
+import com.google.android.gms.vision.CameraSource;
 import com.google.android.gms.vision.barcode.BarcodeDetector;
 
 import it.jaschke.alexandria.R;
@@ -15,6 +18,20 @@ import it.jaschke.alexandria.R;
  */
 public class BarcodeCaptureActivity extends Activity {
     private static final String LOG_TAG = BarcodeCaptureActivity.class.getSimpleName();
+
+    private static final int HANDLE_GMS = 9001;
+    private static final int HANDLE_CAMERA_PERMISSION = 2;
+
+    private static final String autoFocus = "AutoFocus";
+    private static final String useFlash = "UseFlash";
+    private static final String barcodeObject = "Barcode";
+
+    private CameraSource cameraSource;
+    private CameraSourcePreview cameraSourcePreview;
+    private GraphicOverlay<BarcodeGraphic> overlay;
+
+    private ScaleGestureDetector scaleGestureDetector;
+    private GestureDetector gestureDetector;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
