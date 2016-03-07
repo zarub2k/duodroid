456dc28823df1d5b7ef41725cd5f50daa720d11f
Thamizharasu <zarub2k@gmail.com>
2016-03-07 23:12:51 +0530

    Barcode graphic code introduced

--- alexandria/app/src/main/java/it/jaschke/alexandria/changes/BarcodeGraphic.java
+++ alexandria/app/src/main/java/it/jaschke/alexandria/changes/BarcodeGraphic.java
@@ -1,13 +1,81 @@
 package it.jaschke.alexandria.changes;
 
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.RectF;
+
+import com.google.android.gms.vision.barcode.Barcode;
 
 /**
  * @author tham
  */
-public class BarcodeGraphic implements GraphicOverlay.Graphic {
+public class BarcodeGraphic extends GraphicOverlay.Graphic {
+    private static final String LOG_TAG = BarcodeGraphic.class.getSimpleName();
+
+    private static final int[] colors = {
+            Color.BLUE,
+            Color.CYAN,
+            Color.GREEN
+    };
+
+    private int id;
+
+    private static int currentColorIndex = 0;
+
+    private Paint rectPaint;
+    private Paint textPaint;
+
+    private volatile Barcode barcode_;
+
+    public BarcodeGraphic(GraphicOverlay overlay) {
+        super(overlay);
+
+        currentColorIndex = (currentColorIndex + 1) % colors.length;
+        final int selectedColor = colors[currentColorIndex];
+
+        rectPaint = new Paint();
+        rectPaint.setColor(selectedColor);
+        rectPaint.setStyle(Paint.Style.STROKE);
+        rectPaint.setStrokeWidth(4.0f);
+
+        textPaint = new Paint();
+        textPaint.setColor(selectedColor);
+        textPaint.setTextSize(36.0f);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Barcode getBarcode() {
+        return barcode_;
+    }
+
+    void updateItem(Barcode barcode) {
+        barcode_ = barcode;
+        postInvalidate();
+    }
+
     @Override
     public void draw(Canvas canvas) {
-        
+        Barcode barcode = barcode_;
+        if (barcode == null) {
+            return;
+        }
+
+        final RectF rect = new RectF(barcode.getBoundingBox());
+        rect.left = translateX(rect.left);
+        rect.top = translateY(rect.top);
+
+        rect.right = translateX(rect.right);
+        rect.bottom = translateY(rect.bottom);
+
+        canvas.drawRect(rect, rectPaint);
+        canvas.drawText(barcode.rawValue, rect.left, rect.bottom, textPaint);
     }
 }
