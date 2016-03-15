ed94ff0e1ecc89ad68da32ad5c9411c4e50c4d48
Thamizharasu <zarub2k@gmail.com>
2016-03-15 23:07:02 +0530

    un used imports removed

--- alexandria/app/src/main/java/it/jaschke/alexandria/changes/BarcodeCaptureActivity.java
+++ alexandria/app/src/main/java/it/jaschke/alexandria/changes/BarcodeCaptureActivity.java
@@ -6,8 +6,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.pm.PackageManager;
-import android.hardware.Camera;
-import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.util.Log;
